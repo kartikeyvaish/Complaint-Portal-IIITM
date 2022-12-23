a96650d2c2ac09ce2e40f4ec34a73320a21ea8d0
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-12-23 14:20:07 +0530

    :art: code refractored/cleanup

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -1,3 +1,10 @@
+// Packages imports
+import { Request } from "express";
+import mongoose from "mongoose";
+
+// Local imports
+import { complaintDepartmentFilters, statusFilters } from "../config/Constants";
+
 /** 
  * * function to check if a email is valid student email or not (for IIITM Gwalior)
  * - It should start with bcs, imt, img, cn
@@ -32,4 +39,31 @@ export function isValidStudentEmail(email: string) {
 // Function to add leading zeros to a number
 export function addLeadingZeros(num: number, totalLength: number) {
     return String(num).padStart(totalLength, '0');
+}
+
+// function to get filters
+/** 
+ * * function to get filters from request body
+ * @returns {Object} count, afterIdFilter, complaint_department, status
+ */
+export function getFilters(req: Request) {
+    let count = req.body.count ? parseInt(req.body.count.toString()) : 10;
+    let after = req.body.after ? req.body.after.toString() : null;
+
+    // Filter to apply after body
+    const afterThis_id = req.body?.after ? new mongoose.Types.ObjectId(req.body.after.toString()) : null;
+    let afterIdFilter = after ? { _id: { $lt: afterThis_id } } : {};
+
+    // if complaint_department is not null, then filter based on complaint_department
+    let complaint_department = req.body.complaint_department ? complaintDepartmentFilters[req.body.complaint_department] : null;
+
+    // if status is not null, then filter based on status
+    let status = req.body.status ? statusFilters[req.body.status] : null;
+
+    return {
+        count,
+        afterIdFilter,
+        complaint_department,
+        status
+    }
 }
